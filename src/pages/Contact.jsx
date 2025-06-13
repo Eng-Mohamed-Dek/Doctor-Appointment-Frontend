d28ceb416df6c799f0d989e42698a76a94269793
Hirkaab Academy <madaaledesigner80@gmail.com>
2025-06-13 13:40:13 +0300

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
         <div className='flex flex-col justify-center items-start gap-6 leading-6'>
           <p className='font-semibold text-lg text-gray-600'>OUR OFFICE</p>
           <p className='text-gray-500'>1027 Jaalle Siyaad <br /> WX_Xalane, Wadajir, Mogadisho</p>
-          <p className='text-gray-500'>Tel: +(252) 4 459-166 <br /> Email: madaaledesigner@gmail.com</p>
+          <p className='text-gray-500'>Tel: +(252) 4 459-166 <br /> Email: mohamed34@gmail.com</p>
           <p  className='font-semibold text-lg text-gray-600'>Careers at Priscrepto</p>
           <p className='text-gray-500'>Learn more about our team and job openning</p>
           <p className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500 cursor-pointer rounded-full'>Explore Job</p>
